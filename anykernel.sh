2debada960f35efa1171640cecd0b318f2dfeb59
hxsyzl <hxsyzl@ust.edu.kg>
2026-02-03 06:47:29 +0000

    update

--- anykernel.sh
+++ anykernel.sh
@@ -37,6 +37,36 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 
 ## AnyKernel install
 dump_boot;
-write_boot;
+
+# Check if erofs is present in /vendor/etc/fstab.qcom
+# If erofs exists, flash dtbo.img; otherwise skip it
+check_and_flash_dtbo() {
+  local fstab_file="/vendor/etc/fstab.qcom";
+  local has_erofs=0;
+  
+  ui_print " " "Checking for erofs in $fstab_file...";
+  
+  if [ -f "$fstab_file" ]; then
+    if grep -q -i "erofs" "$fstab_file"; then
+      has_erofs=1;
+      ui_print " " "erofs detected! Flashing dtbo.img...";
+    else
+      ui_print " " "No erofs found. Skipping dtbo.img...";
+    fi
+  else
+    ui_print " " "Warning: $fstab_file not found. Skipping dtbo.img...";
+  fi
+  
+  if [ "$has_erofs" -eq 1 ]; then
+    flash_generic dtbo;
+  fi
+}
+
+# Repack and flash boot image
+repack_ramdisk;
+flash_boot;
+
+# Conditionally flash dtbo based on erofs detection
+check_and_flash_dtbo;
 ## end install
 
